b4e0085fa75015cb1597169d796da1a86b25327b
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2019-11-09 10:32:06 +0000

    task: decaffeinate terminus

--- spec/terminus-view-spec.coffee
+++
@@ -1,5 +0,0 @@
-TerminusView = require '../lib/terminus-view'
-
-describe "TerminusView", ->
-  it "has one valid test", ->
-    expect("life").toBe "easy"
---
+++ spec/terminus-view-spec.js
@@ -0,0 +1,8 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+const TerminusView = require('../lib/terminus-view');
+
+describe("TerminusView", () => it("has one valid test", () => expect("life").toBe("easy")));
